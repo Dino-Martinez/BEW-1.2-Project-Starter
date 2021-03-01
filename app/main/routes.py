1e44dd8694da54c85ee81af4a3173f1183da5fc5
Dino-Martinez <>
2021-03-01 15:52:27 -0800

    auth routes and forms done

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,5 +1,22 @@
-from flask import Blueprint
+"""Import packages and modules."""
+from flask import Blueprint, request, render_template, redirect, url_for, flash
+from flask_login import login_user, logout_user, login_required, current_user
+from datetime import date, datetime
+from app.models import Player, Team, Coach, User
+# from app.main.forms import BookForm, AuthorForm, GenreForm
+from app import bcrypt
 
-main = Blueprint('main', __name__)
+# Import app and db from events_app package so that we can run app
+from app import app, db
 
-# Create your routes here.
+main = Blueprint("main", __name__)
+
+##########################################
+#           Routes                       #
+##########################################
+
+
+@main.route('/')
+def homepage():
+    all_users = User.query.all()
+    return render_template('home.html', all_users=all_users)
--- app/models.py
+++ app/models.py
@@ -1,1 +1,69 @@
-# Create your models here.
+"""Create database models to represent tables."""
+from app import db
+from sqlalchemy.orm import backref
+from flask_login import UserMixin
+
+class Team(db.Model):
+    """Team model."""
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    wins = db.Column(db.Integer, nullable=False)
+    losses = db.Column(db.Integer, nullable=False)
+    players = db.relationship('Player', back_populates='team')
+    coaches = db.relationship('Coach', back_populates='team')
+    usersFavoriting = db.relationship('User', back_populates='team')
+
+    def __str__(self):
+        return f'{self.name}:\nWin/Loss:{self.wins}/{self.losses}'
+
+    def __repr__(self):
+        return f'<Team: {self.name}>'
+
+class Player(db.Model):
+    """Player model."""
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    position = db.Column(db.String(80), nullable=False)
+
+    # What team this player is on
+    team_id = db.Column(db.Integer, db.ForeignKey('team.id'), nullable=False)
+    team = db.relationship('Team', back_populates='players')
+
+    def __str__(self):
+        return f'{self.name}:\nPosition:{self.position}'
+
+    def __repr__(self):
+        return f'<Player: {self.name}>'
+
+class Coach(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    position = db.Column(db.String(80), nullable=False)
+    wins = db.Column(db.Integer, nullable=False)
+    losses = db.Column(db.Integer, nullable=False)
+
+    # What team this player is on
+    team_id = db.Column(db.Integer, db.ForeignKey('team.id'), nullable=False)
+    team = db.relationship('Team', back_populates='coaches')
+
+    def __str__(self):
+        return f'{self.name}:\nPosition:{self.position}'
+
+    def __repr__(self):
+        return f'<Coach: {self.name}>'
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False, unique=True)
+    password = db.Column(db.String(200), nullable=False)
+
+    # What is this users favorite team
+    team_id = db.Column(db.Integer, db.ForeignKey('team.id'))
+    team = db.relationship('Team', back_populates='usersFavoriting')
+
+    def __str__(self):
+        return f'{self.username}: {self.team.name}'
+
+    def __repr__(self):
+        return f'<User: {self.username}>'
